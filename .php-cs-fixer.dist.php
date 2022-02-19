7555477f52c21ab2ea5a2a3e9682f54d6f42955b
Anthony Rappa <rappa819@gmail.com>
2022-02-19 15:32:59 -0500

    Fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,12 +5,8 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->notPath('storage/*')
     ->notPath('resources/view/mail/*')
     ->in([
-        __DIR__ . '/app',
         __DIR__ . '/config',
-        __DIR__ . '/database/factories',
-        __DIR__ . '/database/seeders',
-        __DIR__ . '/resources/lang',
-        __DIR__ . '/routes',
+        __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
