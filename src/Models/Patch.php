6eb9d8b98f0ff4f3ee2591bd0e1bc8636f61fa19
rappasoft <rappasoft@users.noreply.github.com>
2023-02-24 04:47:31 +0000

    Fix Styling

--- src/Models/Patch.php
+++ src/Models/Patch.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Patch extends Model
 {
-
     /**
      * @var bool
      */
--- src/Patch.php
+++ src/Patch.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\DB;
  */
 abstract class Patch
 {
-
     /**
      * @var array
      */
